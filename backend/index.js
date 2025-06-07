e312edebcff20e3afe882bee4e2bea6db41729ad
Mayur <ghulem66@gmail.com>
2025-06-07 13:14:22 +0530

    error resolving 1

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,12 @@ const PORT = process.env.PORT || 8080;
 app.get('/ping', (req, res) => {
     res.send('PONG');
 });
+app.use(cors({
+  origin: 'https://expense-tracker-orry-aty3lqchc-mayurs-projects-f058cfbf.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+app.options('*', cors());
 
 app.use(bodyParser.json());
 app.use(cors());
